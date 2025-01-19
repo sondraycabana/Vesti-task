7ecf3eafb57fd3aafd5c6228970c2793aa0c4dc6
sondraycabana <sondraycabana@gmail.com>
2025-01-19 23:53:41 +0000

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:vesti/constants/app_colors.dart';
-import 'package:vesti/constants/app_texts.dart';
-import 'package:vesti/constants/asset_paths.dart';
-import 'package:vesti/widgets/buildCategory.dart';
-import 'package:vesti/widgets/buildInfoChips.dart';
-import 'package:vesti/widgets/buildSection.dart';
 
 import 'Views/onboard/onboardScreen.dart';
 
@@ -20,12 +14,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      // home: HomeScreen(),
-      // home: OnboardingScreen2(),
       home: OnboardingScreen1(),
     );
   }
